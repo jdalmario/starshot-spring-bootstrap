2abb3fe3f9df12afeaf8ee1fc2f4f126ac2f1abe
Jose Almario <jdalmario@gmail.com>
2019-05-24 14:30:16 +0800

    apply authentication filter

--- src/main/java/com/starshot/AuthenticationFilter.java
+++ src/main/java/com/starshot/AuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.starshot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +12,8 @@ import java.io.IOException;
 
 @WebFilter("/*")
 public class AuthenticationFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -20,7 +25,13 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpSession session = httpRequest.getSession();
 
-        chain.doFilter(request, response);
+        if (httpRequest.getRequestURI().endsWith("login")) {
+            chain.doFilter(request, response);
+        } else if (session.getAttribute("username") == null) {
+            httpResponse.sendRedirect("login");
+        } else {
+            chain.doFilter(request, response);
+        }
     }
 
     @Override
